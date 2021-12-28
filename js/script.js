03f01d3c4f18cb2985370f44c0696080f4a0e3ca
Dillon King <Dillonk2012@gmail.com>
2021-12-28 12:45:57 -0600

    modified css for healthbars

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function Player(name, attack, defense) {
+    this.name = name;
+    this.attack = attack;
+    this.defense = defense;
+
+    attack () {
+        
+    }
+}
+
+function Enemy(name, attack, defense) {
+    this.name = name;
+    this.attack = attack;
+    this.defense = defense;
+}
