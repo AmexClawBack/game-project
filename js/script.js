20742598f59ffa174932875f5e4516658a540f2d
Dillon King <Dillonk2012@gmail.com>
2021-12-30 19:58:16 -0600

    Completed the attack method.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 //Defining all variables point to the different DIVs
 let enemyHealth = document.querySelector('.eFont');
 let playerHealth = document.querySelector('.pFont');
-let cardOne = document.querySelector('.card1');
-let CardTwo = document.querySelector('.card2');
-let cardThree = document.querySelector('.card3');
-let CardFour = document.querySelector('.card4');
-let cardFive = document.querySelector('.card5');
-let CardSix = document.querySelector('.card6');
+
+//Unused variables; Identified for later use
+// let cardOne = document.querySelector('.card1');
+// let CardTwo = document.querySelector('.card2');
+// let cardThree = document.querySelector('.card3');
+// let CardFour = document.querySelector('.card4');
+// let cardFive = document.querySelector('.card5');
+// let CardSix = document.querySelector('.card6');
 let attackBtn = document.querySelector('.btn1');
 
 // Player Name and Health
@@ -14,8 +16,8 @@ class Player {
     constructor (name, health) {
     this.name = name;
     this.health = health;
-    }
-}
+    };
+};
 
 // Card Name and Stats
 class Card{
@@ -23,47 +25,37 @@ class Card{
     this.name = cardName;
     this.attack = attack;
     this.defense = defense;
-}   
-    attack(target) {
-       Player.health = target.defense - this.attack;
-       console.log(Player.health)
-    }
-}
+};
+ 
+};
 
 // Identifying between players and cards
 class EnemyHealth extends Player {
     constructor(name, health){
         super(name, health)
-    }
-}
+    };
+};
 class PlayerHealth extends Player {
     constructor(name, health){
         super(name, health)
-    }
-}
+    };
+};
 
 class EnemyCards extends Card {
     constructor(cardName, attack, defense) {
         super(cardName, attack, defense)
-    }
-}
+    };
+};
 class PlayerCards extends Card {
     constructor(cardName, attack, defense) {
         super(cardName, attack, defense)
-    }
-}
+    };
+};
 
 //Settings for both players and their cards
-const enemyDeck = [new EnemyCards('Zalto', 7, 3), new EnemyCards('Cavalier', 6, 5), new EnemyCards('Bloodpyre', 4, 1) ];
-// const enemy01 = new EnemyCards('Zalto', 7, 3); 
-// const enemy02 = new EnemyCards('Cavalier', 6, 5);
-// const enemy03 = new EnemyCards('Bloodpyre', 4, 1);
-
-const playerDeck = [new PlayerCards('Yargle', 9, 3), new PlayerCards('Extractor', 5, 5), new PlayerCards('Archfiend', 6, 6)];
-// const player01 = new PlayerCards('Yargle', 9, 3);
-// const player02 = new PlayerCards('Extractor', 5, 5);
-// const player03 = new PlayerCards('Archfiend', 6, 6);
-
+const enemyDeck = [new EnemyCards('Zalto, Fire Giant Duke', 7, 3), new EnemyCards('Cavalier of Flame', 6, 5), new EnemyCards('Bloodpyre Elemental', 4, 1) ];
+const playerDeck = [new PlayerCards('Yargle, Glutton of Urborg', 9, 3), new PlayerCards('Extractor Demon', 5, 5), new PlayerCards('Archfiend of Spite', 6, 6)];
+// Player and Enemy health
 const userHealth = new PlayerHealth('Player', 20);
 const opponentHealth = new EnemyHealth('Opponent', 20);
 
@@ -73,58 +65,87 @@ const opponentHealth = new EnemyHealth('Opponent', 20);
 //Attack function
 function attack() {
 
-    // Generates random player and enemy cards
     // Found this solution on Progamiz.com
+    // Random card generated for opponent
     function randomOpponent(arr) {
         const randomIndex = Math.floor(Math.random() * arr.length);
         const item = arr[randomIndex];
         return item;
-    }
+    };
     const enemyResult = randomOpponent(enemyDeck);
-    console.log(enemyResult)
     
-    
+    //Random card generated for user
     function randomPlayer(arr) {
         const randomIndex = Math.floor(Math.random() * arr.length);
         const item = arr[randomIndex];
         return item;
-    }
+    };
     const playerResult = randomPlayer(playerDeck);
-    console.log(playerResult)
-
 
     // Outcome of the attacking and defending player, which will be inflicted onto enemy health
-    
     const playerAttackOutcome = playerResult.attack - enemyResult.defense;
-   
     const enemyAttackOutcome = enemyResult.attack - playerResult.defense;
-    console.log(playerAttackOutcome);
-    console.log(enemyAttackOutcome);
+    
+    //playerattack and enemyattack greater than enemydefense and playerdefense
+    if (playerResult.attack > enemyResult.defense && enemyResult.attack > playerResult.defense) {
 
-    enemyHealth.innerText = opponentHealth.health -= playerAttackOutcome;
-    
-    playerHealth.innerText = userHealth.health -= enemyAttackOutcome;
-
-    
-    
-    // Enemy health
-    
-    
-
-    // The actual attack sequence when button is pressed
-    if (playerResult.attack > enemyResult.defense) {
-        // Enemy health
-    
-        //  enemyHealth.innerHtml = "Enemy Health:" + enemyHealth-duelResult;
-        console.log('You have destroyed the enemys defending creature and dealt X damage to enemies health');
-    } else if (playerResult.attack === enemyResult.defense) {
-        console.log("You have destroyed the defending creature, but did not do any damage to your opponent")
+            //Outcome of the battle to display on health amount
+            enemyHealth.innerText = opponentHealth.health -= playerAttackOutcome;
+            playerHealth.innerText = userHealth.health -= enemyAttackOutcome;
         
-    } else  
-        console.log('The opponent is too powerful for you, better luck next time.')
+        //Alert to describe battle participants
+        alert(`A fierce battle ensues between your creature: ${playerResult.name} and opponents' creature: ${enemyResult.name}!`)
+
+        //Alert to describe the battle outcome
+        alert(`Your creature: ${playerResult.name}, attack overpowers ${enemyResult.name}'s defense, causing ${playerAttackOutcome} damage to opponents health. Your opponents' creature: ${enemyResult.name}, attack overpowers ${playerResult.name}'s defense, causing ${enemyAttackOutcome} damage to your health.` )
+        
+    // player attack greater than enemy defense AND enemy attack less than player defense
+    }; if(playerResult.attack > enemyResult.defense && enemyResult.attack < playerResult.defense){
+
+        //Outcome of the battle to display on health amount
+        enemyHealth.innerText = opponentHealth.health -= playerAttackOutcome;
+        
+
+        //Alert to describe battle participants
+        alert(`A fierce battle ensues between your creature: ${playerResult.name} and opponents' creature: ${enemyResult.name}!`)
+
+        //Alert to describe the battle outcome
+        alert(`Your creature: ${playerResult.name}, attack overpowers ${enemyResult.name}'s defense, causing ${playerAttackOutcome} damage to opponents health. Your opponents' creature: ${enemyResult.name}, is unable to overpower ${playerResult.name}'s defense, you lose no health.` );
+
+        //Player attack greater than enemy defense and enemy attack equal to player defnese
+    }; if(playerResult.attack > enemyResult.defense && enemyResult.attack === playerResult.defense){
+
+        //Outcome of the battle to display on health amount
+        enemyHealth.innerText = opponentHealth.health -= playerAttackOutcome;
+    
+        //Alert to describe battle participants
+        alert(`A fierce battle ensues between your creature: ${playerResult.name} and opponents' creature: ${enemyResult.name}!`);
+
+        //Alert to describe the battle outcome
+        alert(`Your creature: ${playerResult.name}, attack overpowers ${enemyResult.name}'s defense, causing ${playerAttackOutcome} damage to opponents health. Your opponents' creature: ${enemyResult.name}, is unable to overpower ${playerResult.name}'s defense, you lose no health.` );
+
+    };if(playerResult.attack === enemyResult.defense && enemyResult.attack > playerResult.defense){
+    
+        //Outcome of the battle to display on health amount      
+        playerHealth.innerText = userHealth.health -= enemyAttackOutcome;
+
+        //Alert to describe battle participants
+        alert(`A fierce battle ensues between your creature: ${playerResult.name} and opponents' creature: ${enemyResult.name}!`)
+
+        //Alert to describe the battle outcome
+        alert(`Your creature: ${playerResult.name}, attack overpowers ${enemyResult.name}'s defense, causing ${playerAttackOutcome} damage to opponents health. Your opponents' creature: ${enemyResult.name}, is unable to overpower ${playerResult.name}'s defense, you lose no health.` )
+    
+    }};
+    
+    //Set the win/lose condition
+    if (enemyHealth.innerText <= 0) {
+        console.log("working")
+        alert(`You have defeated your opponent. Press ${attackBtn} to restart the game`)
+    }; if (userHealth.health <= 0) {
+        console.log("working")
+        alert(`You have been defeated by your opponent. Press ${attackBtn} to restart the game`)
+    } else {
+
     };
 
-    if (opponentHealth.health <= 0) {
-        alert("You have won!")
-        console.log('You have won!')
-    };
+
